808fbe1fb542aedf8a244ed2c609795fe4161d10
Patrick Pan <jinfeng.pan@31huiyi.com>
2016-06-02 18:32:03 +0800

    front end init

---
+++ src/uBlog/webpack.config.js
@@ -0,0 +1,55 @@
+﻿import webpack from 'webpack';
+import path from 'path';
+
+export default {
+    debug: true,
+    devtool:'cheap-module-eval-source-map',
+    entry: [
+        'eventsource-polyfill',//necessary for hot reloading with IE
+        'webpack-hot-middleware/client?reload=true',//note that it reloads the page if hot module reloading fails.
+        './src/index'
+    ],
+    target:'web',
+    output: {
+        path: path.join(__dirname, "./wwwroot"), //Note:Physical files are only output by the production build task 'npm run build'.
+        publicPath:'/',
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: "./src",
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                include:path.join(__dirname, 'src'),
+                loaders:['babel']
+            },
+            {
+                test:/(\.css)$/,
+                loaders:['style', 'css']
+            },
+            {
+                test:/\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader:"file"
+            },
+            {
+                test:/\.(woff|woff2)$/,
+                loader:"url?prefix=font/&limit=5000"
+            },
+            {
+                test:/\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader:"url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test:/\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader:"url?limit=10000&mimetype=image/svg+xml"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+
+};
