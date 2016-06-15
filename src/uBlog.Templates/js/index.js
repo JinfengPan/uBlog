5cc01726dd76ee08f57b0be195fd63779028c6d0
Patrick Pan <jinfeng.pan@31huiyi.com>
2016-06-15 17:11:24 +0800

    首页模板

---
+++ src/uBlog.Templates/js/index.js
@@ -0,0 +1,9 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+import '../node_modules/tether/dist/css/tether.css';
+
+import '../css/index.css';
+
+import 'font-awesome-webpack';
+
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
