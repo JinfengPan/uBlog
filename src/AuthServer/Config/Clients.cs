546fbb36daa4e8850b1a0e17e70edb377fa274d5
patrickfly <flypatrickg@163.com>
2016-06-22 07:41:32 +0800

    d

---
+++ src/AuthServer/Config/Clients.cs
@@ -0,0 +1,45 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthServer.Config
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "ublogmvc",
+                    ClientName="uBlog (MVC)",
+                    Flow = Flows.ClientCredentials,
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret ("123".Sha256() )
+                    },
+                    AllowAccessToAllScopes = true
+                },
+                new Client
+                {
+                    ClientId = "ublogauthcode",
+                    ClientName = "uBlog (Authorization Code)",
+                    Flow = Flows.AuthorizationCode,
+                    AllowAccessToAllScopes = true,
+                    //用来返回token或者auth code
+                    RedirectUris = new List<string>
+                    {
+                        "localhost:8080/STSCallback"
+                    },
+                    ClientSecrets = new List<Secret>()
+                    {
+                         new Secret ("123".Sha256() )
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ src/uBlog.mvc/Helpers/UBlogHttpClient.cs
@@ -0,0 +1,65 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace uBlog.mvc.Helpers
+{
+    public class UBlogHttpClient
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public UBlogHttpClient(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+
+            var accessToken = RequestAccessTokenClientCredentials();
+            client.SetBearerToken(accessToken);
+
+            client.BaseAddress = new Uri("localhost:8081/api");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
+            return client;
+        }
+
+        private string RequestAccessTokenClientCredentials()
+        {
+            var cookieKey = "CCAccessTokenCookie";
+            //CCAccessTokenCookie: Client Credential Access Token Cookie
+            var accessToken = httpContextAccessor.HttpContext.Request.Cookies[cookieKey];
+
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                return accessToken;
+            }
+
+            //create a Token Client
+            //TokenClient is used to easily access the token endpoint.
+            var tokenClient = new TokenClient(
+                "tokenEndpoint",
+                "ublogmvc",//clientId
+                "123" //client secret
+                );
+
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("gallerymanagment").Result;
+
+            //将access token写入cookie
+            httpContextAccessor.HttpContext.Response.Cookies.Append(cookieKey, tokenResponse.AccessToken, new CookieOptions
+            {
+                Expires = DateTime.Now.AddMonths(6)
+            });
+
+            return tokenResponse.AccessToken;
+        }
+    }
+}
